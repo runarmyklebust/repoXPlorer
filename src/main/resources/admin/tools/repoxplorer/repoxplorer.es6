b23409b76d404b56116e4c80fb1dcdc845e0b7ef
trond <trond.hundal@bouvet.no>
2020-10-24 18:57:56 +0200

    Adapt for xp7, Add freemarker. Add transpiling. Add jitpack to build.gradle.

---
+++ src/main/resources/admin/tools/repoxplorer/repoxplorer.es6
@@ -0,0 +1,31 @@
+import { render } from '/site/lib/tineikt/freemarker';
+const portal = require('/lib/xp/portal');
+
+export function get() {
+    const view = resolve('repoxplorer.html');
+    const model = {
+        jsUrl: portal.assetUrl({path: "/js/main.js"}),
+        assetsUrl: portal.assetUrl({path: ""}),
+        repoLoaderService: getServiceUrl('repo-loader-service'),
+        repoInfoService: getServiceUrl('repo-info-service'),
+        deleteRepoServiceUrl: getServiceUrl('delete-repo-service'),
+        createRepoServiceUrl: getServiceUrl('create-repo-service'),
+        createBranchServiceUrl: getServiceUrl('create-branch-service'),
+        queryRepoServiceUrl: getServiceUrl('query-repo-service'),
+        browseServiceUrl: getServiceUrl('browse-service'),
+        autocompleteServiceUrl: getServiceUrl('autocomplete-service'),
+        deleteNodeServiceUrl: getServiceUrl('delete-node-service'),
+        deleteBranchServiceUrl: getServiceUrl('delete-branch-service')
+    };
+
+    return {
+        contentType: 'text/html',
+        body: render(view, model)
+    };
+}
+
+function getServiceUrl(name) {
+    return portal.serviceUrl({
+        service: name
+    })
+}
--- src/main/resources/admin/tools/repoxplorer/repoxplorer.js
+++ src/main/resources/admin/tools/repoxplorer/repoxplorer.js
@@ -1,38 +1,42 @@
-var mustache = require('/lib/mustache');
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.get = get;
+
+var _freemarker = require("/site/lib/tineikt/freemarker");
+
 var portal = require('/lib/xp/portal');
-var repoLib = require('/lib/xp/repo');
 
+function get() {
+  var view = resolve('repoxplorer.html');
+  var model = {
+    jsUrl: portal.assetUrl({
+      path: "/js/main.js"
+    }),
+    assetsUrl: portal.assetUrl({
+      path: ""
+    }),
+    repoLoaderService: getServiceUrl('repo-loader-service'),
+    repoInfoService: getServiceUrl('repo-info-service'),
+    deleteRepoServiceUrl: getServiceUrl('delete-repo-service'),
+    createRepoServiceUrl: getServiceUrl('create-repo-service'),
+    createBranchServiceUrl: getServiceUrl('create-branch-service'),
+    queryRepoServiceUrl: getServiceUrl('query-repo-service'),
+    browseServiceUrl: getServiceUrl('browse-service'),
+    autocompleteServiceUrl: getServiceUrl('autocomplete-service'),
+    deleteNodeServiceUrl: getServiceUrl('delete-node-service'),
+    deleteBranchServiceUrl: getServiceUrl('delete-branch-service')
+  };
+  return {
+    contentType: 'text/html',
+    body: (0, _freemarker.render)(view, model)
+  };
+}
 
-exports.get = function (req) {
-
-    var view = resolve('repoxplorer.html');
-
-
-    var model = {
-        jsUrl: portal.assetUrl({path: "/js/main.js"}),
-        assetsUrl: portal.assetUrl({path: ""}),
-        repoLoaderService: getServiceUrl('repo-loader-service'),
-        repoInfoService: getServiceUrl('repo-info-service'),
-        deleteRepoServiceUrl: getServiceUrl('delete-repo-service'),
-        createRepoServiceUrl: getServiceUrl('create-repo-service'),
-        createBranchServiceUrl: getServiceUrl('create-branch-service'),
-        queryRepoServiceUrl: getServiceUrl('query-repo-service'),
-        browseServiceUrl: getServiceUrl('browse-service'),
-        autocompleteServiceUrl: getServiceUrl('autocomplete-service'),
-        deleteNodeServiceUrl: getServiceUrl('delete-node-service'),
-        deleteBranchServiceUrl: getServiceUrl('delete-branch-service')
-    };
-
-    return {
-        contentType: 'text/html',
-        body: mustache.render(view, model)
-    };
-
-};
-
-var getServiceUrl = function (name) {
-
-    return portal.serviceUrl({
-        service: name
-    })
-};
+function getServiceUrl(name) {
+  return portal.serviceUrl({
+    service: name
+  });
+}
